51482520e9c97933efb5bea1c3ddd5764c8e743c
kakeru-site <75102452+kakeru-site@users.noreply.github.com>
2026-09-23 10:40:35 +0900

    Add files via upload

---
+++ Service worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'muscle-log-shell-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 同じオリジン（index.html本体など）だけをキャッシュ対象にする。
+// Firebase / Chart.js / Googleフォントなど外部リソースはservice workerを介さず通常通りネットワークへ流す。
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, resClone)).catch(() => {});
+        return res;
+      })
+      .catch(() =>
+        caches.match(req).then((cached) => cached || caches.match('./index.html'))
+      )
+  );
+});
